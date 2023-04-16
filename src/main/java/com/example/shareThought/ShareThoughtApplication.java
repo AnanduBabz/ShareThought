edd197dc4058fe42272494b28717cfaaf0486154
AzureAD\AnanduBabu <anandu.babu@bourntec.com>
2023-04-16 18:31:45 +0530

    initial commit

---
+++ src/main/java/com/example/shareThought/ShareThoughtApplication.java
@@ -0,0 +1,13 @@
+package com.example.shareThought;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareThoughtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareThoughtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shareThought/ShareThoughtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shareThought;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareThoughtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
